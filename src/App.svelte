56e2a6d461b40d0f04b6b63712975182b2526e04
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-29 12:13:39 +0100

    right-left

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,21 @@
 <script lang="ts">
-	import Runner from './Runner.svelte';
+	import HtmlRenderTest from "./components/HtmlRenderTest.svelte";
+	import Runner from "./components/Runner.svelte";
 
 	const message = "Welcome to this Svelte site.";
 	const image = "images/moliere.png";
-	const htmlMessage = "This is a <b>test</b> with HTML in a string variable."
 </script>
 
 <main>
 	<h1 class="text-green-900 text-3xl mb-6">Svelte Showcase Site</h1>
 	<p>{message.toUpperCase()}</p>
-	<img class="rounded-lg mt-3 rotate-3" src={image} alt="Moliere" />
-	<Runner/>
-	<p class="mt-3">{htmlMessage}</p>
-	<p class="mt-3">{@html htmlMessage}</p>
+	<section class="flex gap-6">
+		<section>
+			<img class="rounded-lg mt-3 rotate-3" src={image} alt="Moliere" />
+			<Runner />
+		</section>
+		<HtmlRenderTest />
+	</section>
 </main>
 
 <style>
---
+++ src/components/HtmlRenderTest.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	const htmlMessage = "This is a <b>test</b> with HTML in a string variable.";
+</script>
+
+<fieldset class="border border-gray-400 mt-5 rounded w-fit py-2 px-3 h-fit">
+	<legend class="ml-3 font-mono text-gray-500">HTML Render Test</legend>
+	<p class="mb-3">{htmlMessage}</p>
+	<p class="mb-3">{@html htmlMessage}</p>
+</fieldset>
