e742276dbf6d857e55bb1455b61104034e65ec76
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-29 11:42:51 +0100

    text

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,8 @@
+<script lang="ts">
+	const message = "Welcome to this Svelte site."
+</script>
+
 <main>
 	<h1 class="text-green-900 text-3xl mb-6">Svelte Showcase Site</h1>
-	<p>Welcome to this site.</p>
+	<p>{message.toUpperCase()}</p>
 </main>
