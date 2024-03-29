11b2ee6b5b5ef2a1587933f6d9710145d1b84004
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-29 23:11:17 +0100

    range

--- src/components/Ex010Bindings.svelte
+++ src/components/Ex010Bindings.svelte
@@ -1,17 +1,28 @@
 <script lang="ts">
-	let comment = "";
+	let comment = "none";
+	let range = 5;
 
-const initialFocus = (el: HTMLInputElement) => {
-	el.focus();
-}
+	$:rangeMessage = `${range} (${(range / 25 * 100).toFixed(0)}%)`;
+
+	const initialFocus = (el: HTMLInputElement) => {
+		el.focus();
+	};
 </script>
 
 <fieldset
 	class="border border-gray-400 mt-5 rounded w-fit px-3 pb-3 h-fit flex flex-col justify-center"
 >
 	<legend class="font-mono text-gray-500">Ex010Bindings</legend>
-	<div class="bg-yellow-300 p-3 rounded mt-1">
+	<div class="bg-yellow-300 p-3 rounded mt-1 mb-1">
 		Comment: <input type="text" bind:value={comment} use:initialFocus />
 		<p class="font-mono mt-2 bg-yellow-200">{comment}</p>
+	</div>
+	<div class="bg-yellow-300 p-3 rounded mt-1">
+		<div class="flex gap-2">
+			<div>Ranking: 0</div>
+			<input type="range" min="0" max="25" bind:value={range} /> 
+			<div>25</div>
+		</div>
+		<p class="font-mono mt-2 bg-yellow-200">{rangeMessage}</p>
 	</div>
 </fieldset>
