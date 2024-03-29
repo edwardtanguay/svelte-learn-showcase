ca214bb7dedaa5a5dc6bb6775568c3712e05ed1c
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-29 13:40:15 +0100

    interactive

--- src/components/InteractiveTest.svelte
+++ src/components/InteractiveTest.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
 	let message = "hello";
+	$: decoratedMessage = () => {
+		const rand = Math.floor(Math.random() * 10) + 1;
+		return `(((***${message.toUpperCase()}***))) rand=${rand}`;
+	};
 
 	const changeMessage = () => {
-		message = 'okok';
-	}
+		message = "okok";
+	};
 </script>
 
 <fieldset class="border border-gray-400 mt-5 rounded w-fit px-3 h-fit">
 	<legend class="font-mono text-gray-500">Interactive Test</legend>
 	<p class="mb-2">{message}</p>
+	<p class="mb-2">{@html decoratedMessage()}</p>
 	<p class="mb-2">
-		<button on:click={changeMessage} class="bg-gray-300 py-1 px-3 text-black rounded"
-			>change text</button
+		<button
+			on:click={changeMessage}
+			class="bg-gray-300 py-1 px-3 text-black rounded">change text</button
 		>
 	</p>
 </fieldset>
