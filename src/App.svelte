3904531faf12bd07aeb8d49835962de59b98ec61
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-29 14:47:37 +0100

    base each

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import Header from "./components/Header.svelte";
+	import EachBlock from "./components/EachBlock.svelte";
+import Header from "./components/Header.svelte";
 	import HtmlRenderTest from "./components/HtmlRenderTest.svelte";
 	import InteractiveArray from "./components/InteractiveArray.svelte";
 	import InteractiveTest from "./components/InteractiveTest.svelte";
@@ -20,6 +21,7 @@
 			<InteractiveTest />
 			<InteractiveArray />
 			<InteractiveArray nums={[5,6,7]} />
+			<EachBlock/>
 		</section>
 	</section>
 </main>
---
+++ src/components/EachBlock.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	const colors = ["red", "green", "blue"];
+</script>
+
+<fieldset class="border border-gray-400 mt-5 rounded w-fit px-3 h-fit">
+	<legend class="font-mono text-gray-500">Each Block</legend>
+	<p>There are {colors.length} colors.</p>
+</fieldset>
