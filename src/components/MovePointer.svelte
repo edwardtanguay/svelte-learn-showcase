684a098ff110ab03e703baa53a5389012ff716ad
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-29 19:51:09 +0100

    pointer

--- src/components/MovePointer.svelte
+++ src/components/MovePointer.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
-	const message = "the pointer";
+	let message = "move the mouse<br/>in this area";
+
+	const handlePointerMove = (e: PointerEvent) => {
+		message = `mouse is at<br/>${e.clientX} x ${e.clientY}`;
+	};
 </script>
 
-<fieldset class="border border-gray-400 mt-5 rounded w-fit px-3 h-fit">
+<fieldset
+	class="border border-gray-400 mt-5 rounded w-[15rem] px-3 h-[15rem] flex place-items-center"
+	on:pointermove={handlePointerMove}
+>
 	<legend class="font-mono text-gray-500">Move Pointer</legend>
-	<p class="mb-2">{message}</p>
+	<p class="mb-2 w-full text-center">{@html message}</p>
 </fieldset>
