0530e035aa7325447f7eb1dfd1f15feb903470f4
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-29 15:57:08 +0100

    fetching console.log

--- src/components/AsyncTest.svelte
+++ src/components/AsyncTest.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import { getEmployees } from "../tools";
 
-	const message = getEmployees();
+	(async () => {
+		const employees = await getEmployees();
+		console.log(employees);
+	})();
 </script>
 
 <fieldset class="border border-gray-400 mt-5 rounded w-fit px-3 h-fit">
 	<legend class="font-mono text-gray-500">HTML Render Test</legend>
-	<p class="mb-2">{message}</p>
 </fieldset>
--- src/tools.ts
+++ src/tools.ts
@@ -1,3 +1,15 @@
-export const getEmployees = () => {
-	return 'nnn';
-}
+export const getEmployees = async () => {
+	return new Promise((resolve, reject) => {
+		setTimeout(async () => {
+			const response = await fetch(
+				"https://edwardtanguay.vercel.app/share/employees.json"
+			);
+			if (response.ok) {
+				const employees = response.json();
+				resolve(employees);
+			} else {
+				reject("Error fetching data.");
+			}
+		}, 2000);
+	});
+};
