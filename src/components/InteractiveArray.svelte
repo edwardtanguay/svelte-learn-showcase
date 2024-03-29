af357fa890e12b02eda9bcac2fa93fa7fd4431ff
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-29 14:01:52 +0100

    update array

--- src/components/InteractiveArray.svelte
+++ src/components/InteractiveArray.svelte
@@ -1,18 +1,25 @@
 <script lang="ts">
-	let message = "hello";
+	let nums = [9, 2, 1, 4];
 
-	const changeMessage = () => {
-		message = "okok";
+	const addNumber = () => {
+		nums = [...nums, Math.floor(Math.random() * 10) + 1];
 	};
+
+	$: sum = nums.reduce((sum, num) => {
+		sum += num;
+		return sum;
+	}, 0);
 </script>
 
 <fieldset class="border border-gray-400 mt-5 rounded w-fit px-3 h-fit">
-	<legend class="font-mono text-gray-500">Interactive Test</legend>
-	<p class="mb-2">{message}</p>
+	<legend class="font-mono text-gray-500">Interactive Test #2</legend>
+	<p class="mb-2">{nums.join(", ")}</p>
+	<p class="mb-2">sum = {sum}</p>
 	<p class="mb-2">
 		<button
-			on:click={changeMessage}
-			class="bg-gray-300 py-1 px-3 text-black rounded">change text</button
+			on:click={addNumber}
+			class="bg-gray-300 py-1 px-3 text-black rounded"
+			>add random number</button
 		>
 	</p>
 </fieldset>
