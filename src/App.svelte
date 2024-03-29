0535a647048f47fe888d58ac0dc6ef1f38476b0a
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-29 14:53:51 +0100

    color

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import EachBlock from "./components/EachBlock.svelte";
-import Header from "./components/Header.svelte";
+	import Header from "./components/Header.svelte";
 	import HtmlRenderTest from "./components/HtmlRenderTest.svelte";
 	import InteractiveArray from "./components/InteractiveArray.svelte";
 	import InteractiveTest from "./components/InteractiveTest.svelte";
@@ -17,11 +17,11 @@ import Header from "./components/Header.svelte";
 			<Runner />
 		</section>
 		<section>
+			<EachBlock />
 			<HtmlRenderTest />
 			<InteractiveTest />
 			<InteractiveArray />
-			<InteractiveArray nums={[5,6,7]} />
-			<EachBlock/>
+			<InteractiveArray nums={[5, 6, 7]} />
 		</section>
 	</section>
 </main>
--- src/components/EachBlock.svelte
+++ src/components/EachBlock.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-	const colors = ["red", "green", "blue"];
+	const colors = ["red", "green", "blue", "orange", "purple"];
 </script>
 
-<fieldset class="border border-gray-400 mt-5 rounded w-fit px-3 h-fit">
+<fieldset class="border border-gray-400 mt-5 rounded w-fit px-3 pb-3 h-fit">
 	<legend class="font-mono text-gray-500">Each Block</legend>
-	<p>There are {colors.length} colors.</p>
+	{#each colors as color}
+		<p class="font-bold" style="color: {color}">{color}</p>
+	{/each}
 </fieldset>
