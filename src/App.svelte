17048941a19e29fe242823faf3ff694af15bd8b5
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-29 12:03:41 +0100

    html render

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
-	const message = "Welcome to this Svelte site."
+	import Runner from './Runner.svelte';
+
+	const message = "Welcome to this Svelte site.";
+	const image = "images/moliere.png";
+	const htmlMessage = "This is a <b>test</b> with HTML in a string variable."
 </script>
 
 <main>
 	<h1 class="text-green-900 text-3xl mb-6">Svelte Showcase Site</h1>
 	<p>{message.toUpperCase()}</p>
+	<img class="rounded-lg mt-3 rotate-3" src={image} alt="Moliere" />
+	<Runner/>
+	<p class="mt-3">{htmlMessage}</p>
+	<p class="mt-3">{@html htmlMessage}</p>
 </main>
+
+<style>
+	img {
+		border: 5px solid goldenrod;
+	}
+</style>
---
+++ src/Runner.svelte
@@ -0,0 +1,3 @@
+<div>
+	<img class="-rotate-3 rounded-lg border-4 border-green-700" src="images/pheidippides.png" alt="runner"/>
+</div>
