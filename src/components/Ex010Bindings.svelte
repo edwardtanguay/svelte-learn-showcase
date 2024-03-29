ce9aab5c4de39139bf1873c33575d2280f42d2ef
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-29 22:57:59 +0100

    comment finished

--- src/components/Ex010Bindings.svelte
+++ src/components/Ex010Bindings.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
-	let comment = 'none';
+	let comment = "";
+
+const initialFocus = (el: HTMLInputElement) => {
+	el.focus();
+}
 </script>
 
 <fieldset
 	class="border border-gray-400 mt-5 rounded w-fit px-3 pb-3 h-fit flex flex-col justify-center"
 >
 	<legend class="font-mono text-gray-500">Ex010Bindings</legend>
-	Comment: <input type="text" bind:value={comment}/>
-	<p>{comment}</p>
+	<div class="bg-yellow-300 p-3 rounded mt-1">
+		Comment: <input type="text" bind:value={comment} use:initialFocus />
+		<p class="font-mono mt-2 bg-yellow-200">{comment}</p>
+	</div>
 </fieldset>
