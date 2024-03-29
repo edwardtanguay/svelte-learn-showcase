7aaf01f5dd63844ad42b9dcb6327177d91914b09
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-29 14:57:13 +0100

    show colors

--- src/components/EachBlock.svelte
+++ src/components/EachBlock.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	const colors = ["red", "green", "blue", "orange", "purple"];
+	const colors = ["red", "green", "blue", "brown", "purple"];
 </script>
 
 <fieldset class="border border-gray-400 mt-5 rounded w-fit px-3 pb-3 h-fit">
 	<legend class="font-mono text-gray-500">Each Block</legend>
-	{#each colors as color}
-		<p class="font-bold" style="color: {color}">{color}</p>
+	{#each colors as color, index}
+		<p class="font-bold" style="color: {color}">{index}: {color}</p>
 	{/each}
 </fieldset>
