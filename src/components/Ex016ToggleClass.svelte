23c002978805c966e55fae13d61b193a87206e0b
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-02 01:15:16 +0200

    toggle works

--- src/components/Ex016ToggleClass.svelte
+++ src/components/Ex016ToggleClass.svelte
@@ -7,5 +7,25 @@
 >
 	<legend class="font-mono text-gray-500">Ex016ToggleClass</legend>
 
-	<p>toggle test</p>
+	<div
+		class="card font-mono flex items-center justify-center"
+		class:highlight={highlighted}
+	>
+		<p>Toggle Test</p>
+	</div>
+	<button
+		class="bg-gray-300 px-2 pt-1 pb-2 rounded"
+		on:click={() => (highlighted = !highlighted)}>toggle</button
+	>
 </fieldset>
+
+<style>
+	.card {
+		width: 10rem;
+		height: 10rem;
+		background-color: #ccc;
+	}
+	.highlight {
+		background-color: yellow;
+	}
+</style>
