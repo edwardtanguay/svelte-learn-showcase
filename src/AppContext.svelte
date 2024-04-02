8da0a0a881c4739aa7d67d53b5677e078c9462d5
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-02 22:38:46 +0200

    local function works

--- src/AppContext.svelte
+++ src/AppContext.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import { setContext } from "svelte";
-	import { writable} from "svelte/store";
+	import { writable } from "svelte/store";
+	import { appStore } from "./stores";
 
 	let nums = writable([1, 2, 3, 4]);
 	let appTitle = writable("Main App");
-	
+
 	setContext<string>("appTitle", $appTitle);
 	setContext<number[]>("nums", $nums);
-	
+
 	// 2024-04-02 could not get appContext to work as in React
 	// it only works as read-only
 	// it couldn't pass the variable through to the component as writable
@@ -18,12 +19,16 @@
 	};
 
 	setContext<(num: number) => void>("addNumber", (num) => {
-		console.log('in here');
+		console.log("in here");
 		$nums.push(num);
 		console.log($nums);
 	});
 	setContext<(appTitle: string) => void>("changeAppTitle", changeApptitle);
+
+	// export const changeSiteName = (siteName: string) => {
+	// 	$appStore.siteName = "nnn";
+	// };
 </script>
 
-<button on:click={() => changeApptitle('ksjdfksdfj')}>test</button>
+<button on:click={() => changeApptitle("ksjdfksdfj")}>test</button>
 <slot />
--- src/components/Ex020UseStore.svelte
+++ src/components/Ex020UseStore.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
 	import { appStore } from "../stores";
+	// import changeSiteName from '../storeManager.svelte';
+	import { getContext } from "svelte";
+
+	// const changeSiteName = getContext("changeSiteName");
+
+	export const changeSiteName = (siteName: string) => {
+		$appStore.siteName = "nnn";
+	};
 </script>
 
 <div>
 	<p>site name: {$appStore.siteName}</p>
+	<button on:click={() => changeSiteName('nnn')}>test</button>
 </div>
