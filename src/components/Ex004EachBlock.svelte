b0c20467a5613bf13c0c9a568ef306196b458f93
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-02 02:26:24 +0200

    spelling error fixed

--- src/components/Ex004EachBlock.svelte
+++ src/components/Ex004EachBlock.svelte
@@ -13,28 +13,28 @@
 </script>
 
 <div class="pt-2">
-{#if selectedColor !== "" && colors.length !== 0}
-	<p class="mb-3">
-		Choosen color: <span class="font-bold" style="color: {selectedColor}"
-			>{selectedColor}</span
-		>
-	</p>
-{/if}
-<div class="flex gap-3">
-	{#each colors as color, index}
+	{#if selectedColor !== "" && colors.length !== 0}
+		<p class="mb-3">
+			Chosen color: <span class="font-bold" style="color: {selectedColor}"
+				>{selectedColor}</span
+			>
+		</p>
+	{/if}
+	<div class="flex gap-3">
+		{#each colors as color, index}
+			<button
+				class="bg-gray-300 font-bold py-1 px-3 rounded"
+				on:click={() => chooseColor(color)}
+				style="color: {color}">{index}: {color}</button
+			>
+		{/each}
+	</div>
+	{#if colors.length !== 0}
 		<button
-			class="bg-gray-300 font-bold py-1 px-3 rounded"
-			on:click={() => chooseColor(color)}
-			style="color: {color}">{index}: {color}</button
+			class="bg-gray-300 font-bold py-1 px-3 rounded mt-3"
+			on:click={() => deleteFirstColor()}>delete first color</button
 		>
-	{/each}
-</div>
-{#if colors.length !== 0}
-	<button
-		class="bg-gray-300 font-bold py-1 px-3 rounded mt-3"
-		on:click={() => deleteFirstColor()}>delete first color</button
-	>
-{:else}
-	<p>no colors left</p>
-{/if}
+	{:else}
+		<p>no colors left</p>
+	{/if}
 </div>
