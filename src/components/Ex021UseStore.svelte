2d013a8a19f1ad38f064f08fec685609ce601965
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-03 01:12:09 +0200

    extra file

--- src/components/Ex021UseStore.svelte
+++ src/components/Ex021UseStore.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { appStore } from "../stores";
+	// import {removeEmployee} from '../storeFunctions.svelte';
 
 	const removeEmployee = () => {
 		const _appStore = structuredClone($appStore);
@@ -11,5 +12,7 @@
 <div>
 	<p>this site name capitalized is {$appStore.siteName.toUpperCase()}</p>
 	<p>There are {$appStore.employees.length} employees.</p>
-	<button class="bg-gray-300 px-2 pb-1 rounded" on:click={removeEmployee}>remove one</button>
+	<button class="bg-gray-300 px-2 pb-1 rounded" on:click={removeEmployee}
+		>remove one</button
+	>
 </div>
---
+++ src/storeFunctions.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { appStore } from "./stores";
+
+	export const removeEmployee = () => {
+		const _appStore = structuredClone($appStore);
+		_appStore.employees.pop();
+		$appStore = structuredClone(_appStore);
+	};
+</script>
