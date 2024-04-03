363f7ac27423e7de87975f231b7e1901a6ff2e15
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-03 17:46:45 +0200

    click to count

--- src/components/GameBox.svelte
+++ src/components/GameBox.svelte
@@ -1,11 +1,33 @@
-<div class="box"><p>box</p></div>
+<script lang="ts">
+	import { emojis } from "../appData";
+
+	let emoji = "";
+
+	const getNewEmoji = () => {
+		const index = Math.floor(Math.random() * emojis.length);
+		emoji = emojis[index];
+		return null;
+	};
+
+	getNewEmoji();
+</script>
+
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div class="box cursor-pointer select-none" on:click={getNewEmoji}>
+	<p>{emoji}</p>
+</div>
 
 <style lang="scss">
+	$backgroundColor: pink;
 	.box {
 		display: flex;
 		justify-content: center;
 		align-items: center;
-		background-color: pink;
+		background-color: $backgroundColor;
 		font-size: 3vmin;
+		&:hover {
+			background-color: darken($backgroundColor, 10%);
+		}
 	}
 </style>
--- src/routes/PageGame.svelte
+++ src/routes/PageGame.svelte
@@ -1,12 +1,14 @@
 <script>
 	import GameInfoBar from "../components/GameInfoBar.svelte";
 	import GamePlayArea from "../components/GamePlayArea.svelte";
+
+	let footerMessage = "Click to Count";
 </script>
 
 <div class="w-full flex justify-center mt-6">
 	<div>
 		<GameInfoBar title="The Game 🚩" />
 		<GamePlayArea />
-		<GameInfoBar title="Play for Fun" />
+		<GameInfoBar title={footerMessage} />
 	</div>
 </div>
