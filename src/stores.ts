bde7df814b630a50a72f3a6c74ec72f68c8d4df5
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-03 18:50:53 +0200

    solve top-level await issue

--- src/stores.ts
+++ src/stores.ts
@@ -5,10 +5,16 @@ import type { AppStore, Cart } from "./types";
 
 export const cart = writable<Cart>({ cartId: 0, products: [] });
 
-export const appStore = writable<AppStore>({
+// employees (avoids top-level await requirement since this won't compile)
+const appStore = writable<AppStore>({
 	siteName: "The Main Site",
-	employees: await getEmployees(),
+	employees: []
 });
+(async () => {
+	const _employees = await getEmployees();
+	appStore.update((s) => ({ ...s, employees: _employees }));
+})();
+export { appStore };
 
 export let gameFooterMessage = writable<string>("Click to Count");
 export let gameNumberOfTimesClicks = writable<number>(0);
